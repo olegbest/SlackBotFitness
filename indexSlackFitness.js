fc09fdcfb5743088a78db3d5049c1dd09b8a7e13
olegbest <pashkevich.oleg@inbox.ru>
2018-09-17 21:50:34 +0300

    Slack

--- indexSlackFitness.js
+++ indexSlackFitness.js
@@ -2,16 +2,15 @@ const fs = require("fs");
 const Slack = require('slack')
 const token = "xoxb-343426990707-433422170887-galBx6XB0WurQLz7LZUkl3Pa";
 const bot = new Slack({token});
-
+let info = require('./info');
 
 let onlineUsers = [];
 let userOffline = [];
 
 setInterval(async function () {
-    let info = require('./info');
+
     let date = new Date();
-    console.log(date)
-    if (date.getHours() > 8 && date.getHours() < 20 && date.getDay() < 6) {
+    if (date.getHours() > 5 && date.getHours() < 17 && date.getDay() < 6) {
         onlineUsers = [];
         userOffline = [];
         let channels = await bot.channels.list();
@@ -37,12 +36,12 @@ setInterval(async function () {
             let ex = info.main[Math.floor(Math.random() * info.main.length)];
             console.log(ex)
             if (userDo) {
-                // console.log(userDo)
-                // bot.chat.postMessage({
-                //     token,
-                //     channel: "CA3KJ6VBP",
-                //     text: "<@" + userDo.id + ">" + " выполняй " + ex.name.toUpperCase() + " " + ex.value + " раз"
-                // });
+                console.log(userDo)
+                bot.chat.postMessage({
+                    token,
+                    channel: "CA3KJ6VBP",
+                    text: "<@" + userDo.id + ">" + " выполняй " + ex.name.toUpperCase() + " " + ex.value + " раз"
+                });
             }
             // let offlineText = "";
             // userOffline.forEach((us)=>{
@@ -58,17 +57,13 @@ setInterval(async function () {
         }, 1000)
 
     }
-}, 5000);
+}, 40 * 60 * 1000);
 
 setInterval(() => {
     let date = new Date();
     if (date.getDay() < 6) {
-        let file_info = JSON.parse(fs.readFileSync("./info.json", "utf8"));
-        // console.log(file_info);
-        file_info.main.forEach((el) => {
+        info.main.forEach((el) => {
             el.value++;
         });
-
-        fs.writeFileSync("./info.json", JSON.stringify(file_info));
     }
 }, 12 * 60 * 60 * 1000)
